290a137d8fb125ebdab62644e8fa1dcb0ee38e26
Alex Ahumada <projectsperminute@gmail.com>
2023-07-20 21:08:09 +0200

    feat: Add forms with validation and dynamic buttons

--- actions/actions.py
+++ actions/actions.py
@@ -42,15 +42,18 @@ from dotenv import load_dotenv
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-from rasa_sdk import Action, Tracker
-from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, FormValidationAction, Tracker
+from rasa_sdk.events import SlotSet, EventType, AllSlotsReset
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
 
 load_dotenv()
 
-my_config = Config(
-    region_name=os.getenv("AWS_REGION"),
-)
+# my_config = Config(
+#     region_name=os.getenv("AWS_REGION"),
+# )
+
+ALLOWED_FILE_FORMATS = ["csv", "xls", "xlsx", "pdf"]
 
 aggregate_titles = ["total", "totales"]
 
@@ -69,9 +72,95 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     return response.choices[0].message["content"]
 
 
-class ActionFetchData(Action):
+class ValidateDataSearchTermsForm(FormValidationAction):
     def name(self) -> Text:
-        return "action_fetch_data"
+        return "validate_data_search_terms_form"
+
+    def validate_data_search_terms(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `data_search_terms` value."""
+
+        url_search = f"{os.getenv('DKAN_API')}/search?fulltext={urllib.parse.quote(slot_value.lower())}"
+        response_search = requests.request("GET", url_search, headers={}, data={})
+        response_search_json = response_search.json()
+        num_items = response_search_json["total"]
+
+        # print("url_search in validation:", url_search)
+
+        if num_items == "0":
+            dispatcher.utter_message(text="Lo siento, no tengo datos sobre ese tema.")
+            return {"data_search_terms": None}
+        dispatcher.utter_message(text=f"OK! Quieres datos sobre {slot_value}.")
+        return {"data_search_terms": slot_value}
+
+
+class ValidateDataFileFormatForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_data_file_format_form"
+
+    def validate_data_file_format(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `data_file_format` value."""
+
+        # print("DATA_FILE_FORMAT Validation")
+
+        data_meta = tracker.get_slot("data_meta")
+
+        allowed_file_formats_dynamic = []
+
+        for distribution in data_meta["distribution"]:
+            allowed_file_formats_dynamic.append(distribution["format"].lower())
+
+        if slot_value.lower() not in allowed_file_formats_dynamic:
+            dispatcher.utter_message(text="El formato no es valido.")
+            return {"data_file_format": None}
+        dispatcher.utter_message(
+            text=f"¡Ok! Quieres descargar archivos en formato {slot_value}."
+        )
+        return {"data_file_format": slot_value}
+
+
+class AskForDataFileFormat(Action):
+    def name(self) -> Text:
+        return "action_ask_data_file_format"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[EventType]:
+        data_meta = tracker.get_slot("data_meta")
+
+        buttons = []
+
+        for distribution in data_meta["distribution"]:
+            buttons.append(
+                {
+                    "title": distribution["format"].upper(),
+                    "payload": distribution["format"].lower(),
+                }
+            )
+
+        dispatcher.utter_message(
+            text="¿En qué formato quieres los datos?", buttons=buttons
+        )
+        return []
+
+
+class ActionSearchData(Action):
+    def name(self) -> Text:
+        return "action_search_data"
 
     def run(
         self,
@@ -85,8 +174,8 @@ class ActionFetchData(Action):
         # Search dataset with search terms
         search_terms = tracker.get_slot("data_search_terms")
         # print(search_terms)
-        url_search = f"{os.getenv('DKAN_API')}/search?fulltext={urllib.parse.quote(search_terms)}&page=1&page-size=20"
-        # print(url_search)
+        url_search = f"{os.getenv('DKAN_API')}/search?fulltext={urllib.parse.quote(search_terms)}"
+        # print("url_search in action:", url_search)
         payload = {}
         headers = {}
         response_search = requests.request(
@@ -125,17 +214,8 @@ class ActionFetchData(Action):
         )
 
         # Send message as json
-        dispatcher.utter_message(text=f"El dataset {dataset_id} han sido encontrados.")
+        dispatcher.utter_message(text=f"He encontrado el dataset con id: {dataset_id}.")
         # dispatcher.utter_message(json_message=response.json())
-
-        # Ask if user wants to see a plot
-        # dispatcher.utter_button_message(
-        #     text="¿Quieres ver un gráfico?",
-        #     buttons=[
-        #         {"title": "Sí", "payload": "Quiero ver un gráfico"},
-        #         {"title": "No", "payload": "/no"},
-        #     ],
-        # )
 
         return [
             SlotSet("data", response_datastore.json()),
@@ -180,17 +260,18 @@ class ActionPlotData(Action):
         df = pd.json_normalize(data, record_path=["results"])
         # Transform suitable data to numeric or keep as string
         df = df.apply(pd.to_numeric, errors="coerce").fillna(df)
-        # df = pd.read_csv(
-        #     "https://datos.cadiz.local.ddev.site/sites/default/files/uploaded_resources/residuos-recogidos-por-servicios-municipales-v1.0.0.xlsx",
-        #     # "actions/data/residuos-recogidos-por-servicios-municipales-v1.1.2.csv",
-        #     # encoding="utf-8", encoding_errors="ignore",
+
+        # df_csv = pd.read_csv(
+        #     "http://datos.cadiz.local.ddev.site/sites/default/files/uploaded_resources/residuos-recogidos-por-servicios-municipales-v1.0.0.csv",
         #     delimiter=",",
-        #     # encoding="unicode_escape",
         # )
         # print("CSV loaded")
-        # print(df)
-        # print(df.shape)
-        # print(df.info())
+        # print("DATAFRAME:", df)
+        # print("DATAFRAME:", df.shape)
+        # print("DATAFRAME:", df.info())
+        # print("CSV:", df_csv)
+        # print("CSV:", df_csv.shape)
+        # print("CSV:", df_csv.info())
 
         plt.style.use("seaborn")
         plt.figure(figsize=(10, 5))
@@ -281,14 +362,14 @@ class ActionPlotData(Action):
                     },
                     ExpiresIn=3600,
                 )
+                dispatcher.utter_message(
+                    image=presigned_image_url,
+                )
             except ClientError as ce:
                 print("error", ce)
             finally:
                 s3_client.close()
                 # Send image
-                dispatcher.utter_message(
-                    image=presigned_image_url,
-                )
 
                 # # Ask if user wants to see a plot
                 # dispatcher.utter_button_message(
@@ -298,6 +379,67 @@ class ActionPlotData(Action):
                 #         {"title": "No", "payload": "/no"},
                 #     ],
                 # )
+
+        return []
+
+
+class ActionDownloadData(Action):
+    def name(self) -> Text:
+        return "action_download_data"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        # theres a bug with rasa use of aiogram in utter_send_file for some file formats,
+        # pdf works ok but csv and xlsx don't, so we are using the telegram api directly
+        # aiogram.utils.exceptions.WrongFileIdentifier: Wrong file identifier/http url specified
+        # once the bug is fixed we can use the dispatcher.utter_message
+        # dispatcher.utter_message(response="utter_send_file", file_url=document_url)
+
+        conversation_id = tracker.sender_id
+
+        data_meta = tracker.get_slot("data_meta")
+        data_file_format = tracker.get_slot("data_file_format")
+        data_distributions = data_meta["distribution"]
+
+        print("data distributions type", type(data_distributions))
+        print("FILE FORMAT:", data_file_format)
+
+        document_index = None
+        for i, obj in enumerate(data_distributions):
+            print(i, obj["format"])
+            if obj["format"] == data_file_format.lower():
+                document_index = i
+                break
+
+        print("document_index:", document_index)
+
+        request_url = f"https://api.telegram.org/bot{os.getenv('TELEGRAM_API_TOKEN')}/sendDocument"
+        document_url = data_meta["distribution"][document_index]["downloadURL"]
+        filename = os.path.basename(document_url)
+        # TODO: setting verify to False disables SSL verification, which is necessary for our local setup with DDEV
+        # This should be removed in a final release
+        response_document = requests.get(document_url, verify=False)
+
+        if response_document.ok:
+            print("CSV file downloaded successfully.")
+        else:
+            print("Failed to download the CSV file.")
+
+        files = {
+            "document": (filename, response_document.content),
+        }
+        params = {"chat_id": conversation_id, "filename": filename}
+        response = requests.post(request_url, files=files, params=params)
+
+        if response.ok:
+            print("CSV file sent successfully.")
+        else:
+            print("Failed to send the CSV file.")
+            dispatcher.utter_message(text="Error enviando el archivo.")
 
         return []
 
@@ -317,18 +459,25 @@ class ActionExplainData(Action):
 
         # Set OpenAI API key
         openai.api_key = os.getenv("OPENAI_API_KEY")
+        # models = openai.Model.list()
+        # print(models)
 
         data = tracker.get_slot("data")
+        print(data["results"])
         prompt = f"""
         Summarize the dataset in json delimited by triple backticks \ 
         into a single sentence in spanish.
-        ```{data}```
+        ```{data["results"]}```
         """
-
-        # response = get_completion(prompt)
-        # print(response)
-        # dispatcher.utter_message(text=response)
-        dispatcher.utter_message(text="ChatGPT en modo debug (code: 001).")
+        try:
+            response = get_completion(prompt)
+            print(response)
+            dispatcher.utter_message(text=response)
+        except Exception as e:
+            dispatcher.utter_message(
+                text="Ha ocurrido un error al evaluar los datos, el conjunto de datos es demasiado grande."
+            )
+        # dispatcher.utter_message(text="ChatGPT en modo debug (code: 001).")
 
         return []
 
@@ -359,17 +508,22 @@ class ActionStatisticsData(Action):
         ```{data}```
         """
 
-        # response = get_completion(prompt)
-        # print(response)
-        # dispatcher.utter_message(text=response)
-        dispatcher.utter_message(text="ChatGPT en modo debug (code: 002).")
+        try:
+            response = get_completion(prompt)
+            print(response)
+            dispatcher.utter_message(text=response)
+        except Exception as e:
+            dispatcher.utter_message(
+                text="Ha ocurrido un error al evaluar los datos, el conjunto de datos es demasiado grande."
+            )
+        # dispatcher.utter_message(text="ChatGPT en modo debug (code: 002).")
 
         return []
 
 
-class ActionDownloadData(Action):
+class ActionEmptySlots(Action):
     def name(self) -> Text:
-        return "action_download_data"
+        return "action_empty_slots"
 
     def run(
         self,
@@ -377,38 +531,4 @@ class ActionDownloadData(Action):
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
-        # theres a bug with rasa use of aiogram in utter_send_file for some file formats,
-        # pdf works ok but csv and xlsx don't, so we are using the telegram api directly
-        # aiogram.utils.exceptions.WrongFileIdentifier: Wrong file identifier/http url specified
-        # once the bug is fixed we can use the dispatcher.utter_message
-        # dispatcher.utter_message(response="utter_send_file", file_url=document_url)
-
-        conversation_id = tracker.sender_id
-
-        data_meta = tracker.get_slot("data_meta")
-
-        request_url = f"https://api.telegram.org/bot{os.getenv('TELEGRAM_API_TOKEN')}/sendDocument"
-        document_url = data_meta["distribution"][1]["downloadURL"]
-        filename = os.path.basename(document_url)
-        # TODO: setting verify to False disables SSL verification, which is necessary for our local setup with DDEV
-        # This should be removed in a final release
-        response_document = requests.get(document_url, verify=False)
-
-        if response_document.ok:
-            print("CSV file downloaded successfully.")
-        else:
-            print("Failed to download the CSV file.")
-
-        files = {
-            "document": (filename, response_document.content),
-        }
-        params = {"chat_id": conversation_id, "filename": filename}
-        response = requests.post(request_url, files=files, params=params)
-
-        if response.ok:
-            print("CSV file sent successfully.")
-        else:
-            print("Failed to send the CSV file.")
-            dispatcher.utter_message(text="Error enviando el archivo.")
-
-        return []
+        return [AllSlotsReset()]
